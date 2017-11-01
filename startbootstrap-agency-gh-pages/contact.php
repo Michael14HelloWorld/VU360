353a7fd8d5b4be3c9748198c836170274710f3e2
Michael Groff <bgmichael1400@gmail.com>
2017-11-01 17:06:40 +0000

    Fixing.

--- startbootstrap-agency-gh-pages/contact.php
+++ startbootstrap-agency-gh-pages/contact.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = $email;
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'bgmichael1400@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
